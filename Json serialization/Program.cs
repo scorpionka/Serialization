edb5f625875c961ad10145ad875c3445063c366b
Aliaksei Kuzmitski <Aliaksei_Kuzmitski@epam.com>
2022-02-21 23:47:20 +0300

    Add project files.

---
+++ Json serialization/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Json_serialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ XML serialization/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XML_serialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
