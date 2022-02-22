980dc134df31e9d7708cad2d91f62cc89753c49a
Aliaksei Kuzmitski <Aliaksei_Kuzmitski@epam.com>
2022-02-23 00:05:21 +0300

    feat(serialization): implement Binary serialization, XML serialization, Json serialization

--- Json serialization/Program.cs
+++ Json serialization/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace Json_serialization
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            Department department = new Department() { DepartmentName = "Accounting" };
+            department.Employees = new List<Employee> {
+                new Employee() { EmployeeName = "Petrovna" },
+                new Employee() { EmployeeName = "Mar'yaVanna" }
+            };
+
+            using (FileStream fs = new FileStream("department.json", FileMode.OpenOrCreate))
+            {
+                await JsonSerializer.SerializeAsync<Department>(fs, department);
+            }
+
+            using (FileStream fs = new FileStream("department.json", FileMode.OpenOrCreate))
+            {
+#nullable enable
+                Department? newDepartment = await JsonSerializer.DeserializeAsync<Department>(fs);
+#nullable disable
+                Console.WriteLine($"Department name: {newDepartment.DepartmentName}:");
+
+                foreach (Employee employee in newDepartment.Employees)
+                {
+                    Console.WriteLine($"Employee name: {employee.EmployeeName}");
+                }
+            }
         }
     }
 }
---
+++ XML serialization/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace XML_serialization
+{
+    [Serializable]
+    public class Department
+    {
+        [XmlElement(ElementName = "Department")]
+        public string DepartmentName { get; set; }
+        public List<Employee> Employees { get; set; }
+    }
+}
