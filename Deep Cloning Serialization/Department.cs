2044b200b3074367b11fe87155035b4a5d207805
Aliaksei Kuzmitski <Aliaksei_Kuzmitski@epam.com>
2022-02-23 10:36:52 +0300

    feat(Deep Cloning Serialization): implement method

---
+++ Deep Cloning Serialization/Department.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Deep_Cloning_Serialization
+{
+    [Serializable]
+    public class Department
+    {
+        public string DepartmentName { get; set; }
+        public List<Employee> Employees { get; set; }
+    }
+}
